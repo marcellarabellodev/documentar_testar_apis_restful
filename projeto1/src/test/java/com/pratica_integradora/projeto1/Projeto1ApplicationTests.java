7de7a80ba95565a3c5ab4751bfcec604041aae0f
Lucas Teles <lucasouzateles@gmail.com>
2024-11-11 20:04:28 -0300

    :broom: limpando o teste

--- projeto1/src/test/java/com/pratica_integradora/projeto1/Projeto1ApplicationTests.java
+++ projeto1/src/test/java/com/pratica_integradora/projeto1/Projeto1ApplicationTests.java
@@ -1,13 +1,10 @@
 package com.pratica_integradora.projeto1;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+//por enquanto removi a parte dos testes.
 @SpringBootTest
 class Projeto1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
